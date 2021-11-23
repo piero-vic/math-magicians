3d25333576c9db00ef4dc4d1692977d07953a6d8
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-23 10:41:19 -0500

    Style calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import Calculator from './components/Calculator';
+import './App.css';
 
 function App() {
   return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,24 +1,28 @@
+import './Calculator.css';
+
 function Calculator() {
   return (
-    <div className="calculator">
-      <div className="output-bar" />
+    <div className="Calculator">
+      <div className="output-bar">
+        0
+      </div>
       <button type="button">AC</button>
       <button type="button">+/-</button>
       <button type="button">%</button>
-      <button type="button">÷</button>
+      <button className="operation-btn" type="button">÷</button>
       <button type="button">7</button>
       <button type="button">8</button>
       <button type="button">9</button>
-      <button type="button">x</button>
+      <button className="operation-btn" type="button">x</button>
       <button type="button">4</button>
       <button type="button">5</button>
       <button type="button">6</button>
-      <button type="button">-</button>
+      <button className="operation-btn" type="button">-</button>
       <button type="button">1</button>
       <button type="button">2</button>
       <button type="button">3</button>
-      <button type="button">+</button>
-      <button type="button">0</button>
+      <button className="operation-btn" type="button">+</button>
+      <button className="two-col" type="button">0</button>
       <button type="button">.</button>
       <button type="button">=</button>
     </div>
