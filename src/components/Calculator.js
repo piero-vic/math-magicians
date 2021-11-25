9161d43b915fc92de6a509a242cc58e2aeb8243c
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-25 14:31:16 -0500

    Change Calculator from class to function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,58 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import Button from './Button';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-    this.buttonClick = this.buttonClick.bind(this);
-  }
+function Calculator() {
+  const [data, updateData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  buttonClick(buttonName) {
-    this.setState((state) => ({
-      data: calculate(state.data, buttonName),
-    }));
-  }
+  const buttons = ['AC', '+/-', '%', '÷',
+    '7', '8', '9', 'x',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '.', '='];
 
-  render() {
-    const { data } = this.state;
-    const buttons = ['AC', '+/-', '%', '÷',
-      '7', '8', '9', 'x',
-      '4', '5', '6', '-',
-      '1', '2', '3', '+',
-      '0', '.', '='];
-
-    return (
-      <div className="Calculator">
-        <div className="output-bar">
-          {data.next || data.operation || data.total || '0'}
-        </div>
-        {
-          buttons.map((btn) => (
-            <Button
-              key={btn.id}
-              className={
-                btn === '÷' || btn === 'x' || btn === '-' || btn === '+' || btn === '='
-                  ? 'operation-btn'
-                  : `btn${btn}`
-              }
-              data={data}
-              buttonClick={() => this.buttonClick(btn)}
-              name={btn}
-            />
-          ))
-        }
+  return (
+    <div className="Calculator">
+      <div className="output-bar">
+        {data.next || data.operation || data.total || '0'}
       </div>
-    );
-  }
+      {
+        buttons.map((btn) => (
+          <Button
+            key={btn.id}
+            className={
+              btn === '÷' || btn === 'x' || btn === '-' || btn === '+' || btn === '='
+                ? 'operation-btn'
+                : `btn${btn}`
+            }
+            data={data}
+            buttonClick={() => updateData(calculate(data, btn))}
+            name={btn}
+          />
+        ))
+      }
+    </div>
+  );
 }
 
 export default Calculator;
