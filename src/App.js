ff791905265c2ad9f275272b0ccfec42fdf7a9d8
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-30 11:50:08 -0500

    Create homepage and quote page

--- src/App.js
+++ src/App.js
@@ -17,13 +17,56 @@ const App = () => (
         <li><a href="/quote">Quote</a></li>
       </ul>
     </header>
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<h2>This is the home page</h2>} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<h2>This is the quotes page</h2>} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <div className="App">
+            <div className="homepage-container">
+              <h2>Welcome to our page!</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        )}
+      />
+      <Route
+        path="/calculator"
+        element={(
+          <div className="App calculator-container">
+            <h2>Let&apos;s do some Math</h2>
+            <Calculator />
+          </div>
+        )}
+      />
+      <Route
+        path="/quote"
+        element={(
+          <div className="App quote-container">
+            <h2>
+              Mathemathics is not about numbers, equations, computations or algorithms;
+              it&apos;s about undestanding - William Paul Thurston
+            </h2>
+          </div>
+        )}
+      />
+    </Routes>
   </Router>
 );
 
