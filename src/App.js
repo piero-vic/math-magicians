331de4b99837f1c6b9e8e772477c0e66cd4e441b
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-30 11:12:12 -0500

    Set up routes and create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <Calculator />
-  </div>
+  <Router>
+    <header className="header">
+      <h1>Math Magicians</h1>
+      <ul className="navigation">
+        <li><a href="/">Home</a></li>
+        <li><a href="/calculator">Calculator</a></li>
+        <li><a href="/quote">Quote</a></li>
+      </ul>
+    </header>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h2>This is the home page</h2>} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<h2>This is the quotes page</h2>} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
