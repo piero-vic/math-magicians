3b93acad95a066a918df1fe0fbd680b061bca596
codepanther <pezechigozirim@gmail.com>
2021-12-01 22:52:11 +0100

    Add jest snapshots to test Button and Calculator components

---
+++ src/__tests__/Button.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Button from '../components/Button';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Button name="=" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
